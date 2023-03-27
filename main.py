31782c3cc3aed2b20943c29e3f41efa31b98acab
bilal asghar <bilalasghar1964@gmail.com>
2023-03-27 23:41:42 +0500

    admin dashboard

--- main.py
+++ main.py
@@ -4,21 +4,20 @@ from flask_mail import Mail
 import json
 from datetime import datetime
 
-
 with open('config.json', 'r') as c:
     params = json.load(c)["params"]
 
 local_server = True
 app = Flask(__name__)
 app.config.update(
-    MAIL_SERVER = 'smtp.gmail.com',
-    MAIL_PORT = '465',
-    MAIL_USE_SSL = True,
-    MAIL_USERNAME = params['gmail-user'],
-    MAIL_PASSWORD=  params['gmail-password']
+    MAIL_SERVER='smtp.gmail.com',
+    MAIL_PORT='465',
+    MAIL_USE_SSL=True,
+    MAIL_USERNAME=params['gmail-user'],
+    MAIL_PASSWORD=params['gmail-password']
 )
 mail = Mail(app)
-if(local_server):
+if (local_server):
     app.config['SQLALCHEMY_DATABASE_URI'] = params['local_uri']
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = params['prod_uri']
@@ -50,39 +49,43 @@ def home():
     posts = Posts.query.filter_by().all()[0:params['no_of_posts']]
     return render_template('index.html', params=params, posts=posts)
 
-@app.route("/dashboard")
+
+@app.route("/dashboard/", methods=['GET', 'POST'])
 def dashboard():
-    return render_template("login.html", params=params)
+    if request.method == "POST":
+        pass
+    else:
+        return render_template("login.html", params=params)
+    return 'OK'
 
 
-@app.route("/post/", methods=['GET'])
+@app.route("/post/", methods=['GET', 'POST'])
 def post_route(post_slug):
     post = Posts.query.filter_by(slug=post_slug).first()
     return render_template('post.html', params=params, post=post)
+
 
 @app.route("/about")
 def about():
     return render_template('about.html', params=params)
 
 
-@app.route("/contact", methods = ['GET', 'POST'])
+@app.route("/contact", methods=['GET', 'POST'])
 def contact():
-    if(request.method=='POST'):
+    if request.method == 'POST':
         name = request.form.get('name')
         email = request.form.get('email')
         phone = request.form.get('phone')
         message = request.form.get('message')
-        entry = Contacts(name=name, phone_num = phone, msg = message, date= datetime.now(),email = email )
+        entry = Contacts(name=name, phone_num=phone, msg=message, date=datetime.now(), email=email)
         db.session.add(entry)
         db.session.commit()
         mail.send_message('New message from ' + name,
                           sender=email,
-                          recipients = [params['gmail-user']],
-                          body = message + "\n" + phone
+                          recipients=[params['gmail-user']],
+                          body=message + "\n" + phone
                           )
     return render_template('contact.html', params=params)
 
 
 app.run(debug=True)
-
-
